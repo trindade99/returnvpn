188fc4056437a3058fce39854fe2aa1a3d76996c
victor lopes <victortrindadeopes@gmail.com>
2025-05-26 15:53:48 +0100

    macos changes

---
+++ returnvpn/tun_linux.py
@@ -0,0 +1,14 @@
+import pytun
+import os
+
+def create_tun(ifname):
+    tun = pytun.TunTapDevice(name=ifname, flags=pytun.IFF_TUN | pytun.IFF_NO_PI)
+    tun.addr = "10.10.0.1"
+    tun.netmask = "255.255.255.0"
+    tun.mtu = 1500
+    tun.up()
+    return tun
+
+def cleanup_tun(ifname):
+    os.system(f"ip link set {ifname} down")
+    os.system(f"ip tuntap del dev {ifname} mode tun")
--- returnvpn/tunutils.py
+++ returnvpn/tunutils.py
@@ -1,15 +1,8 @@
-# --- retunvpn/tunutils.py ---
-import pytun
-import os
+import sys
 
-def create_tun(ifname):
-    tun = pytun.TunTapDevice(name=ifname, flags=pytun.IFF_TUN | pytun.IFF_NO_PI)
-    tun.addr = "10.10.0.1"
-    tun.netmask = "255.255.255.0"
-    tun.mtu = 1500
-    tun.up()
-    return tun
-
-def cleanup_tun(ifname):
-    os.system(f"ip link set {ifname} down")
-    os.system(f"ip tuntap del dev {ifname} mode tun")
+if sys.platform.startswith("linux"):
+    from .tun_linux import create_tun, cleanup_tun
+elif sys.platform == "darwin":
+    from .tun_macos import create_tun, cleanup_tun
+else:
+    raise NotImplementedError("Unsupported platform")
